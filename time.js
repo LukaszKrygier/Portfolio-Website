c50de8e4d29d435d47a7b7d06ff483d2fcf7fd2f
Lukasz Krygier <144470385+LukaszKrygier@users.noreply.github.com>
2024-10-07 11:38:25 +0200

    Update time.js

--- time.js
+++ time.js
@@ -4,11 +4,11 @@ function updateTime() {
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const ampm = hours >= 12 ? 'AM' : 'PM';
-    const formattedHours = hours % 12 || 12; // Convert 24-hour time to 12-hour time
+    const formattedHours = hours % 12 || 12; 
     const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
 
     clockElement.textContent = `${formattedHours}:${formattedMinutes} ${ampm}`;
 }
 
-setInterval(updateTime, 1000); // Update the time every second
-updateTime(); // Initial call to set the time immediately
+setInterval(updateTime, 1000); 
+updateTime(); 
